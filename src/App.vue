af045eaa874c2e12c3ce38ca5b776ab414a926bf
PROGRAMADOR DIEGUIN <diegorojasrios31@gmail.com>
2024-09-17 15:03:51 -0400

    merge develop

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { useTheme } from 'vuetify'
 import BuyNow from '@core/components/BuyNow.vue'
 import ScrollToTop from '@core/components/ScrollToTop.vue'
 import initCore from '@core/initCore'
@@ -8,6 +7,7 @@ import {
   useConfigStore,
 } from '@core/stores/config'
 import { hexToRgb } from '@layouts/utils'
+import { useTheme } from 'vuetify'
 
 const { global } = useTheme()
 
@@ -23,7 +23,6 @@ const configStore = useConfigStore()
     <!-- ℹ️ This is required to set the background color of active nav link based on currently active global theme's primary -->
     <VApp :style="`--v-global-theme-primary: ${hexToRgb(global.current.value.colors.primary)}`">
       <RouterView />
-      <BuyNow />
       <ScrollToTop />
     </VApp>
   </VLocaleProvider>
