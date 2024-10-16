8b66a4716e7e8b3086223123ddf863e99e5a4396
PROGRAMADOR DIEGUIN <diegorojasrios31@gmail.com>
2024-10-16 16:56:46 -0400

    subi el dist

---
+++ dist/assets/lazy-26c7f9d7.js
@@ -0,0 +1,1 @@
+import{aU as r,ar as n,a2 as u,w as l}from"./index-7410be13.js";const c=r({eager:Boolean},"lazy");function m(e,o){const a=n(!1),s=u(()=>a.value||e.eager||o.value);l(o,()=>a.value=!0);function t(){e.eager||(a.value=!1)}return{isBooted:a,hasContent:s,onAfterLeave:t}}export{c as m,m as u};
