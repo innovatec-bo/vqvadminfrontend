762643322c77dafa4d1c47c7fc3eeea73a22050d
PROGRAMADOR DIEGUIN <diegorojasrios31@gmail.com>
2024-10-17 14:31:42 -0400

    reestructuracion

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,12 @@
 export const COOKIE_MAX_AGE_1_YEAR = 365 * 24 * 60 * 60
 
-export const URL_BACKEND = 'https://phplaravel-1171375-4944906.cloudwaysapps.com/'
-export const URL_MICROSERVICIO= 'http://verticalhomesia.nuna.tech:3000/'
+// export const URL_BACKEND = 'https://phplaravel-1171375-4944906.cloudwaysapps.com/'
+// export const URL_MICROSERVICIO= 'http://verticalhomesia.nuna.tech:3000/'
+
+export const URL_BACKEND = import.meta.env.VITE_URL_BACKEND
+export const URL_MICROSERVICIO = import.meta.env.VITE_URL_MICROSERVICIO
+
+
 
 
 export const URL_ADMIN =  URL_BACKEND +'api/' 
