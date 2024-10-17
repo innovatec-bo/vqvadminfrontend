274ff61f99a4a947226d33490c2d443288eca2e6
PROGRAMADOR DIEGUIN <diegorojasrios31@gmail.com>
2024-10-17 16:14:49 -0400

    BUILD V4

--- dist/assets/VCardText-c2348594.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./VAvatar-79625373.js";const e=t("v-card-text");export{e as V};
---
+++ dist/assets/useGenerateImageVariant-70d4eb0d.js
@@ -0,0 +1,1 @@
+import{a8 as u,aj as i,a2 as f}from"./index-58af6325.js";const m=(n,t,s,o,e=!1)=>{const r=u(),{global:a}=i();return f(()=>{if(a.name.value==="light")return r.skin==="bordered"&&e?s:n;if(a.name.value==="dark")return r.skin==="bordered"&&e?o:t})};export{m as u};
