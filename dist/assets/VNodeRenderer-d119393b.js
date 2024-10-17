4a1a6b16f648c212cdd574b1c2cbbd78dd6f583f
PROGRAMADOR DIEGUIN <diegorojasrios31@gmail.com>
2024-10-17 14:56:50 -0400

    BUILD V3

--- dist/assets/VNodeRenderer-d119393b.js
+++
@@ -1,1 +0,0 @@
-import{M as r}from"./index-42509b9d.js";const n=r({name:"VNodeRenderer",props:{nodes:{type:[Array,Object],required:!0}},setup(e){return()=>e.nodes}});export{n as V};
--- dist/assets/constants-1a2aba86.js
+++
@@ -1,1 +0,0 @@
-const A=31536e3,E={}.VITE_URL_BACKEND,R={}.VITE_URL_MICROSERVICIO,s=E+"api/",_="LAND",I="DEPARTAMENT";export{A as C,I as D,_ as L,s as U,R as a};
