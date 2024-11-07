d990840bb616ac707787ce65fc699a626c9a85ce
PROGRAMADOR DIEGUIN <diegorojasrios31@gmail.com>
2024-11-07 09:17:44 -0400

    Nueva version con Build generado

---
+++ dist/assets/VNodeRenderer-37ca53c6.js
@@ -0,0 +1,1 @@
+import{M as r}from"./index-68d8d6c3.js";const n=r({name:"VNodeRenderer",props:{nodes:{type:[Array,Object],required:!0}},setup(e){return()=>e.nodes}});export{n as V};
---
+++ dist/assets/scopeId-cd403e1e.js
@@ -0,0 +1,1 @@
+import{bk as o}from"./index-68d8d6c3.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
