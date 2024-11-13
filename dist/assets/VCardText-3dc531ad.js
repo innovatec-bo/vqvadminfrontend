ddbaa1037617b1dcab4663fc6262163133ad09f2
PROGRAMADOR DIEGUIN <diegorojasrios31@gmail.com>
2024-11-13 18:47:39 -0400

    Nuevo IA

--- dist/assets/VCardText-3dc531ad.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./VAvatar-c3790b31.js";const e=t("v-card-text");export{e as V};
---
+++ dist/assets/scopeId-2e25374f.js
@@ -0,0 +1,1 @@
+import{bk as o}from"./index-df4ab518.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
