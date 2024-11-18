91275ddad54c7cf0cc7fd0a2d7b8fdbe51540bf1
melanieyl <melanieyupanqui44@gmail.com>
2024-11-18 17:44:46 -0400

    build de cambios

---
+++ dist/assets/VCardText-e63ee757.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-f8e27e7f.js";const e=t("v-card-text");export{e as V};
---
+++ dist/assets/VSpacer-3eabc0b2.js
@@ -0,0 +1,1 @@
+import{c as a}from"./VAvatar-f8e27e7f.js";const e=a("v-spacer","div","VSpacer");export{e as V};
