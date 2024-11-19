3538394e85cfddd9b53031c1f1fa245fe8cb1060
melanieyl <melanieyupanqui44@gmail.com>
2024-11-19 11:34:19 -0400

    dist de los cambios

---
+++ dist/assets/ssrBoot-dd1e392d.js
@@ -0,0 +1,1 @@
+import{ar as t,H as e,a2 as s,at as n}from"./index-68e95a2c.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
