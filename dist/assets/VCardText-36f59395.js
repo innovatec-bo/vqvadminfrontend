6317998a9e6b87ff1701f44ff78af79547ad7c6f
melanieyl <melanieyupanqui44@gmail.com>
2024-11-20 14:42:43 -0400

    cambios en el dist

---
+++ dist/assets/VCardText-36f59395.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-feef9875.js";const e=t("v-card-text");export{e as V};
---
+++ dist/assets/scopeId-d260b0a4.js
@@ -0,0 +1,1 @@
+import{bk as o}from"./index-037a4698.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
