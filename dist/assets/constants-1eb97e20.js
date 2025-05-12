8dfadf07917de73846c0d18d6e308c1df2e1325f
Jair <javier.jair.cussy.saucedo@gmail.com>
2025-05-11 23:59:53 -0400

    Modifed constants in dist/assets

--- dist/assets/constants-1eb97e20.js
+++ dist/assets/constants-1eb97e20.js
@@ -1,1 +1,2 @@
-const t=31536e3,a="http://vayaquevalla.test/",s="http://vayaquevalla.test/",A=a+"api/",E="LAND",R="DEPARTAMENT";export{t as C,R as D,E as L,s as U,A as a};
+const t=31536e3,a="https://backend.vayaquevalla.com/",s="https://backend.vayaquevalla.com/",A=a+"api/",E="LAND",R="DEPARTAMENT";export { A as a, t as C, R as D, E as L, s as U };
+
